d939ce6e0bbe2b3916f45a061bf94dc8adab1677
takuseno <takuma.seno@gmail.com>
2017-07-29 10:15:30 +0900

    Initla commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import uuid
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+
+setup(name="lightsaber",
+        version="0.1",
+        license="MIT",
+        description="Utility scripts for machine learning research",
+        url="https://github.com/imai-laboratory/lightsaber",
+        packages=find_packages())
