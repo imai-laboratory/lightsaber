b80217517df386dddc6b74e4da0e901686884eac
smatsumori <shoya.matsumori@gmail.com>
2017-07-29 14:12:05 +0900

    MOD: moved to log. WIP

--- lightsaber/log/log.py
+++ lightsaber/log/log.py
@@ -7,7 +7,6 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
-from googleapiclient.http import MediaFileUpload
 import glob
 from collections import namedtuple
 
@@ -15,49 +14,106 @@ from collections import namedtuple
 SCOPES = 'https://www.googleapis.com/auth/drive'
 CLIENT_SECRET_FILE = '~/.credentials/client_secret.json'
 APPLICATION_NAME = 'pythonDrive'
+
+# DEPRECATED
 MIMES = {
     '.jpg': 'image/jpeg',
-    '.csv': 'text/plain'
+    '.csv': 'text/plain',
+    '.ss': 'application/vnd.google-apps.spreadsheet'
 }
-EXTENSIONS = ['.jpg', '.csv']
+
+SSMIME = 'application/vnd.google-apps.spreadsheet'
 
 
-class pDrive():
-    def __init__(self, directories, extensions=None, credentials=None,
-                 no_web_auth_flags=False
+class CloudLog():
+    def __init__(self,
+                 root_id,
+                 directory,
+                 log_name,
+                 credentials=None,
+                 no_web_auth_flags=False,
                  ):
-        self.directories = directories
+        self.directory = directory
+        self.log_name = log_name
         self.no_web_auth_flags = no_web_auth_flags
+
+        print('save to:{} (google drive)'.format(
+            os.path.join(self.directory, self.log_name)
+        )
+              )
+
         if no_web_auth_flags:
             self.args = tools.argparser.parse_args()
             self.args.noauth_local_webserver = True
-        if not extensions:
-            self.extensions = EXTENSIONS
-            print('No extensions specified. using {}'.format(EXTENSIONS))
 
         self.credentials = (CLIENT_SECRET_FILE if not credentials else
                             credentials)
 
-        self.file_paths = [os.path.join(d, '*' + e) for d in self.directories
-                           for e in self.extensions]
+        # file_path = test_dir/test_log (spread sheet)
+        self.file_path = os.path.join(self.directory, self.log_name)
 
-        self.filesfromdirs = []
         Filetup = namedtuple('Filetup', 'parent files')
 
-        for path in self.file_paths:
-            self.filesfromdirs.append(Filetup(os.path.dirname(path),
-                                              glob.glob(path)))
-
-        # todo: error handling
-        print('files to load: {}'.format(
-            [d.files for d in self.filesfromdirs if d.files]
+        self.file_from_path = Filetup(
+            os.path.dirname(self.file_path),
+            glob.glob(self.file_path)
         )
-              )
+
         self.credentials = self.get_credentials()
         self.http = self.credentials.authorize(httplib2.Http())
         self.service = discovery.build('drive', 'v3', http=self.http)
 
+        # get service for spread sheet
+        self.ss_service = discovery.build(
+            'sheets',
+            'v4',
+            http=self.http,
+            discoveryServiceUrl=(
+                'https://sheets.googleapis.com/$discovery/rest?',
+                'version=v4'
+            )
+        )
+        # create parent directory
+        self.parent_id = self.create_folders(
+            self.folders_list,
+            self.root_id
+        )
+
+        # create spread sheet
+        self.spread_sheet_id = self.create_spread_sheet(
+            self.parent_id,
+            self.log_name
+        )
+
+    def create_spread_sheet(self, filename, parent_id=None):
+        '''
+            Creates an empty spread sheet in google drive
+        '''
+        mimetype = SSMIME
+        file_metadata = {
+            'title': os.path.split(filename)[-1],
+            'mimeType': mimetype,
+            'parents': [parent_id]
+        }
+
+        request = self.service.files().insert(
+            body=file_metadata
+        ).execute(http=self.http)
+
+        print('CREATING: {:<40}'.format(filename), end=' ... ')
+        sys.stdout.flush()
+
+        file_id = request.get('id')
+        print ('CERATED. Spread Seet ID: {}'.format(
+            file_id
+        )
+               )
+        return file_id
+
     def get_credentials(self):
+        '''
+            get credentials from google platform
+        '''
         home_dir = os.path.expanduser('~')
         credential_dir = os.path.join(home_dir, '.credentials')
 
@@ -84,9 +140,13 @@ class pDrive():
         _filename, extension = os.path.splitext(filename)
         return MIMES[extension]
 
-    def create_dirs(self, dirs, parent_id):
-        if dirs:
-            folder_name = dirs.pop(0)
+    def create_folders(self, path, parent_id):
+        '''
+            A recursive function to create directories
+            path: a list which will be mutable
+        '''
+        if path:
+            folder_name = path.pop(0)
             folder_exists = self.folder_exists(parent_id, folder_name)
 
             if folder_exists:
@@ -95,13 +155,14 @@ class pDrive():
                 print('folder: {} dose not exists'.format(folder_name))
                 new_parent_id = self.create_folder(folder_name, parent_id)
 
-            return self.create_dirs(dirs, new_parent_id)
+            return self.create_folders(path, new_parent_id)
         else:
             return parent_id
 
     def folder_exists(self, parent_id, folder_name):
         '''
-            Check if the folder exists in parent
+            Check if the folder exists in parent and
+            if so return its id
         '''
         query = "\'{}\' in parents".format(parent_id)
         results = self.service.files().list(
@@ -117,6 +178,9 @@ class pDrive():
         return None
 
     def create_folder(self, folder, parent_id):
+        '''
+            Creates new folder under designated parent folder
+        '''
         print("Creating new folder: %s" % (folder))
         file_metadata = {
             'name': folder,
@@ -128,45 +192,29 @@ class pDrive():
         self.sub_folder_id = folder.get('id')
         return self.sub_folder_id
 
-    def upload_file(self, filename, parent_id=None):
-        if not filename:
-            pass
-        mimetype = self.get_mimetype(filename)
-        file_metadata = {
-            'name': os.path.split(filename)[-1],
-            'mimeType': mimetype,
-            'parents': [parent_id]
-        }
-        media = MediaFileUpload(filename,
-                                mimetype=mimetype)
-        print('UPLOADING: {:<40}'.format(filename), end=' ... ')
-        sys.stdout.flush()
-        file = self.service.files().create(body=file_metadata,
-                                           media_body=media,
-                                           fields='id').execute()
-        print ('UPLOADED. File ID: {}'.format(
-            file.get('id')
-        )
-               )
+    def get_spread_sheet(self, spread_sheet_id):
+        result = self.ss_service.spreadsheets().values().get(
+            spreadsheetId=self.spread_sheet_id
+        ).execute()
 
-    def upload_files(self, parent_id):
-        for d in self.filesfromdirs:
-            sub_pid = self.create_dirs(d.parent.replace('./', '').split('/'),
-                                       parent_id)
-            for f in d.files:
-                self.upload_file(f, sub_pid)
+        values = result.get('values', [])
+        if not values:
+            print('No data found.')
 
-    def test(self):
-        """Shows basic usage of the Google Drive API.
-        Creates a Google Drive API service object and outputs the names and IDs
-        for up to 10 files.
-        """
-        results = self.service.files().list(
-            pageSize=10, fields="nextPageToken, files(id, name)").execute()
-        items = results.get('files', [])
-        if not items:
-            print('No files found.')
         else:
-            print('Files:')
-            for item in items:
-                print(u'{0} ({1})'.format(item['name'], item['id']))
+            print('Name, Major:')
+            for row in values:
+                # Print columns A and E, which correspond to indices 0 and 4.
+                print('%s, %s' % (row[0], row[4]))
+
+    def logprint(self, message):
+        """
+            print message to stdout and spread-sheet
+        """
+        print('(LOGGED) {}'.format(message))
+
+
+if __name__ == '__main__':
+    cloud = CloudLog('0Bw2Q2C4IYEfATmRKaUxNTEpvY2M',
+                     'testd1/testd2', 'log_name')
+    cloud.get_spread_sheet(cloud.spread_sheet_id)
