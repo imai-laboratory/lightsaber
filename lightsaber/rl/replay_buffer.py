bd869430bf04d32fe45b3f2e2019fd8e4712abb2
takuseno <takuma.seno@gmail.com>
2017-12-09 12:06:36 +0900

    Add EpisodeReplayBuffer for recurrent network

--- lightsaber/rl/replay_buffer.py
+++ lightsaber/rl/replay_buffer.py
@@ -28,6 +28,57 @@ class ReplayBuffer:
             done.append(experience['done'])
         return obs_t, actions, rewards, obs_tp1, done
 
+class EpisodeReplayBuffer:
+    def __init__(self, episode_size):
+        self.buffer = deque(maxlen=episode_size)
+        self.tmp_obs_t_buffer = []
+        self.tmp_action_buffer = []
+        self.tmp_reward_buffer = []
+        self.tmp_obs_tp1_buffer = []
+        self.tmp_done_buffer = []
+
+    def append(self, obs_t, action, reward, obs_tp1, done):
+        if isinstance(done, bool):
+            done = 1 if done else 0
+        self.tmp_obs_t_buffer.append(obs_t)
+        self.tmp_action_buffer.append(action)
+        self.tmp_reward_buffer.append(reward)
+        self.tmp_obs_tp1_buffer.append(obs_tp1)
+        self.tmp_done_buffer.append(done)
+
+    def end_episode(self):
+        episode = dict(
+            obs_t=self.tmp_obs_t_buffer,
+            action=self.tmp_action_buffer,
+            reward=self.tmp_reward_buffer,
+            obs_tp1=self.tmp_obs_tp1_buffer,
+            done=self.tmp_done_buffer
+        )
+        self.buffer.append(episode)
+        self.reset_tmp_buffer()
+
+    def reset_tmp_buffer(self):
+        self.tmp_obs_t_buffer = []
+        self.tmp_action_buffer = []
+        self.tmp_reward_buffer = []
+        self.tmp_obs_tp1_buffer = []
+        self.tmp_done_buffer = []
+
+    def sample(self, batch_size):
+        episodes = random.sample(self.buffer, batch_size)
+        obt_t = []
+        actions = []
+        rewards = []
+        obs_tp1 = []
+        done = []
+        for episode in episodes:
+            obt_t.append(episode['obt_t'])
+            actions.append(episode['action'])
+            rewards.append(episode['reward'])
+            obs_tp1.append(episode['obs_tp1'])
+            done.append(episode['done'])
+        return obs_t, actions, rewards, obs_tp1, done 
+
 class NECReplayBuffer:
     def __init__(self, buffer_size):
         self.buffer = deque(maxlen=buffer_size)
