a9162225f20657616b4071e8f49a45ee72d7e982
takuseno <takuma.seno@gmail.com>
2018-02-07 01:23:48 +0900

    Add AsyncTrainer

--- lightsaber/rl/trainer.py
+++ lightsaber/rl/trainer.py
@@ -2,6 +2,8 @@ from collections import deque
 import tensorflow as tf
 import numpy as np
 import copy
+import time
+import threading
 
 
 class AgentInterface:
@@ -95,3 +97,79 @@ class Trainer:
 
                 if global_step > self.final_step:
                     return
+
+class AsyncTrainer:
+    def __init__(self, envs, agents, state_shape=[84, 84],
+            final_step=1e7, state_window=1, preprocess=lambda s: s,
+            training=True, render=False, debug=True, before_action=None,
+            after_action=None, end_episode=None, n_threads=10):
+
+        # meta data shared by all threads
+        self.meta_data = {
+            'shared_step': 0,
+            'shared_episode': 0
+        }
+
+        # inserted callbacks
+        def _before_action(state, global_step, local_step):
+            shared_step = self.meta_data['shared_step']
+            if before_action is not None:
+                before_action(state, shared_step, global_step, local_step)
+
+        def _after_action(state, reward, global_step, local_step):
+            self.meta_data['shared_step'] += 1
+            shared_step = self.meta_data['shared_step']
+            if after_action is not None:
+                after_action(state, reward, shared_step, global_step, local_step)
+
+        def _end_episode(reward, global_step, episode):
+            shared_step = self.meta_data['shared_step']
+            self.meta_data['shared_episode'] += 1
+            shared_episode = self.meta_data['shared_episode']
+            if debug:
+                msg = 'global_step: {}, local_step: {}, episode: {}, reward: {}'
+                print(msg.format(
+                    shared_step,
+                    global_step,
+                    episode,
+                    reward
+                ))
+            if end_episode is not None:
+                end_episode(reward, shared_step, global_step, shared_episode, episode)
+
+        self.trainers = []
+        for i in range(n_threads):
+            env = envs[i]
+            agent = agents[i]
+            trainer = Trainer(
+                env=env,
+                agent=agent,
+                state_shape=state_shape,
+                final_step=final_step,
+                state_window=state_window,
+                preprocess=preprocess,
+                training=training,
+                render=i == 0 and render,
+                debug=False,
+                before_action=_before_action,
+                after_action=_after_action,
+                end_episode=_end_episode
+            )
+            self.trainers.append(trainer)
+
+    def start(self):
+        sess = tf.get_default_session()
+        coord = tf.train.Coordinator()
+        # gym renderer is only available on the main thread
+        render_trainer = self.trainers.pop(0)
+        threads = []
+        for i in range(len(self.trainers)):
+            def run(index):
+                with sess.as_default():
+                    self.trainers[index].start()
+            thread = threading.Thread(target=run, args=(i,))
+            thread.start()
+            threads.append(thread)
+            time.sleep(0.1)
+        render_trainer.start()
+        coord.join(threads)
