1745a13862419a8c23eba79de16db3002d7ffa82
takuseno <takuma.seno@gmail.com>
2018-04-17 16:17:54 +0900

    Fix issue for pip >= 10

--- setup.py
+++ setup.py
@@ -2,7 +2,10 @@
 
 import uuid
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try:
+    from pip._internal.req import parse_requirements
+except ImportError:
+    from pip.req import parse_requirements
 
 install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 
